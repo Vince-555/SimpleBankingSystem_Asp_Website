faa7d639880654153776844ed99c7681cd8729ce
Vince-555 <ventsi.d.1@gmail.com>
2021-07-10 17:03:19 +0300

    Project cleaned, ready to start.

---
+++ SimpleBankingSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleBankingSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
